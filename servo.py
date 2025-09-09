aeb33803db2e30c4fe3110ab9bf0c401c35c3d8a
Joaogabriel0207 <149544641+Joaogabriel0207@users.noreply.github.com>
2025-09-09 17:12:10 -0300

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,35 +1,12 @@
 from flask import Flask, request, render_template_string
-from gpiozero import Servo
+from gpiozero import AngularServo
 from time import sleep
 
 app = Flask(__name__)
 
-html_template = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Controle de Servo</title>
-</head>
-<body>
-    <h1>Controle de Servo SG90</h1>
-    <form method="POST" action="/set_angle">
-        <label for="gpio">GPIO (BCM):</label>
-        <input type="number" id="gpio" name="gpio" required><br><br>
-
-        <label for="angle">Ângulo (0 a 180):</label>
-        <input type="number" id="angle" name="angle" min="0" max="180" required><br><br>
-
-        <button type="submit">Enviar</button>
-    </form>
-</body>
-</html>
-"""
+html_template = """..."""  # seu HTML permanece igual
 
 servos = {}
-
-def angle_to_value(angle):
-    # Converte 0-180° para valor -1 a 1 do gpiozero
-    return (angle / 90) - 1
 
 @app.route("/")
 def index():
@@ -41,14 +18,13 @@ def set_angle():
     angle = float(request.form["angle"])
 
     if gpio not in servos:
-        servo = Servo(gpio)
+        servo = AngularServo(gpio, min_angle=0, max_angle=180)
         servos[gpio] = servo
     else:
         servo = servos[gpio]
 
-    servo.value = angle_to_value(angle)
+    servo.angle = angle
     sleep(0.5)
-    servo.detach()  # evita tremores
 
     return f"Servo no GPIO {gpio} ajustado para {angle}°"
 
